22fd39341dab90694c7e7c00b1ed04abfe14f2d9
isdood <calebjdt@gmail.com>
2025-01-04 17:00:55 -0500

    dbg.

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,54 +1,12 @@
 use volatile::Volatile;
-use core::fmt;
+use core::fmt::{self, Write};
 use spin::Mutex;
 use lazy_static::lazy_static;
 
 const BUFFER_HEIGHT: usize = 25;
 const BUFFER_WIDTH: usize = 80;
 
-#[allow(dead_code)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u8)]
-pub enum Color {
-    Black = 0,
-    Blue = 1,
-    Green = 2,
-    Cyan = 3,
-    Red = 4,
-    Magenta = 5,
-    Brown = 6,
-    LightGray = 7,
-    DarkGray = 8,
-    LightBlue = 9,
-    LightGreen = 10,
-    LightCyan = 11,
-    LightRed = 12,
-    Pink = 13,
-    Yellow = 14,
-    White = 15,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(transparent)]
-struct ColorCode(u8);
-
-impl ColorCode {
-    fn new(foreground: Color, background: Color) -> ColorCode {
-        ColorCode((background as u8) << 4 | (foreground as u8))
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(C)]
-struct ScreenChar {
-    ascii_character: u8,
-    color_code: ColorCode,
-}
-
-#[repr(transparent)]
-struct Buffer {
-    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
-}
+// ... (keep Color enum and other structs the same) ...
 
 pub struct Writer {
     column_position: usize,
@@ -65,7 +23,6 @@ impl Writer {
                     self.move_to_next_line();
                 }
 
-                // Always write to the last row
                 let row = BUFFER_HEIGHT - 1;
                 let col = self.column_position;
 
@@ -76,7 +33,7 @@ impl Writer {
 
                 // Clear everything above the last row
                 for r in 0..BUFFER_HEIGHT - 1 {
-                    self.clear_row(r);
+                    self.clear_current_row(r);
                 }
 
                 self.buffer.chars[row][col].write(colored_char);
@@ -85,26 +42,66 @@ impl Writer {
         }
     }
 
-    fn move_to_next_line(&mut self) {
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+
+    pub fn clear_current_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+
+    pub fn move_to_next_line(&mut self) {
         // Clear the row above the last row
         for row in 0..BUFFER_HEIGHT - 1 {
-            self.clear_row(row);
+            self.clear_current_row(row);
         }
         // Clear the last row
-        self.clear_row(BUFFER_HEIGHT - 1);
+        self.clear_current_row(BUFFER_HEIGHT - 1);
         self.column_position = 0;
+    }
+
+    pub fn change_color(&mut self, foreground: Color, background: Color) {
+        self.color_code = ColorCode::new(foreground, background);
+    }
+
+    pub fn backspace(&mut self) {
+        if self.column_position > 0 {
+            self.column_position -= 1;
+            let color_code = self.color_code;
+            let col = self.column_position;
+
+            let row = BUFFER_HEIGHT - 1;
+            self.buffer.chars[row][col].write(ScreenChar {
+                ascii_character: b' ',
+                color_code,
+            });
+        }
     }
 
     pub fn init_cursor_position(&mut self) {
-        // Clear everything
         for row in 0..BUFFER_HEIGHT {
-            self.clear_row(row);
+            self.clear_current_row(row);
         }
-        // Start at the bottom
         self.column_position = 0;
     }
+}
 
-    // ... (keep other methods the same) ...
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
 }
 
 lazy_static! {
@@ -114,7 +111,6 @@ lazy_static! {
             color_code: ColorCode::new(Color::White, Color::Black),
                                                       buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
         };
-        // Initialize cursor at the bottom
         writer.init_cursor_position();
         writer
     });
@@ -122,7 +118,6 @@ lazy_static! {
 
 #[doc(hidden)]
 pub fn _print(args: fmt::Arguments) {
-    use core::fmt::Write;
     use x86_64::instructions::interrupts;
     interrupts::without_interrupts(|| {
         WRITER.lock().write_fmt(args).unwrap();
@@ -130,13 +125,19 @@ pub fn _print(args: fmt::Arguments) {
 }
 
 pub fn set_color(foreground: Color, background: Color) {
-    WRITER.lock().change_color(foreground, background);
+    use x86_64::instructions::interrupts;
+    interrupts::without_interrupts(|| {
+        WRITER.lock().change_color(foreground, background);
+    });
 }
 
 pub fn clear_screen() {
-    let mut writer = WRITER.lock();
-    for row in 0..BUFFER_HEIGHT {
-        writer.clear_row(row);
-    }
-    writer.column_position = 0;
+    use x86_64::instructions::interrupts;
+    interrupts::without_interrupts(|| {
+        let mut writer = WRITER.lock();
+        for row in 0..BUFFER_HEIGHT {
+            writer.clear_current_row(row);
+        }
+        writer.column_position = 0;
+    });
 }
