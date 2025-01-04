5231d5e082b84d549e9c6b2531d028051bfdf331
isdood <calebjdt@gmail.com>
2025-01-04 16:28:39 -0500

    dbg.

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,9 +1,8 @@
 // src/keyboard.rs
-use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1, KeyCode};
 use spin::Mutex;
-use x86_64::instructions::port::Port;
 use crate::{print, vga_buffer};
-use crate::vga_buffer::Color;
+use crate::vga_buffer::{Color, WRITER};
 
 lazy_static::lazy_static! {
     static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
@@ -15,35 +14,29 @@ lazy_static::lazy_static! {
 pub fn add_scancode(scancode: u8) {
     if let Ok(Some(key_event)) = KEYBOARD.lock().add_byte(scancode) {
         if let Some(key) = KEYBOARD.lock().process_keyevent(key_event) {
+            // Set color to green for user input
+            vga_buffer::set_color(Color::Green, Color::Black);
+
+            let mut writer = WRITER.lock();
+
             match key {
                 DecodedKey::Unicode(character) => {
-                    // Set color to green before printing character
-                    vga_buffer::set_color(Color::Green, Color::Black);
                     print!("{}", character);
                 },
-                DecodedKey::RawKey(key) => print!("{:?}", key),
-            }
-        }
-    }
-}
-
-fn handle_keyevent(key: DecodedKey) {
-    let mut writer = WRITER.lock();
-    match key {
-        DecodedKey::Unicode(character) => {
-            writer.write_byte(character as u8);
-        }
-        DecodedKey::RawKey(key) => {
-            match key {
-                KeyCode::ArrowUp => writer.write_string("^"),
-                KeyCode::ArrowDown => writer.write_string("v"),
-                KeyCode::ArrowLeft => writer.write_string("<"),
-                KeyCode::ArrowRight => writer.write_string(">"),
-                _ => (), // Ignore other special keys
+                DecodedKey::RawKey(key) => {
+                    match key {
+                        KeyCode::ArrowUp => writer.write_string("^"),
+                        KeyCode::ArrowDown => writer.write_string("v"),
+                        KeyCode::ArrowLeft => writer.write_string("<"),
+                        KeyCode::ArrowRight => writer.write_string(">"),
+                        _ => print!("{:?}", key),
+                    }
+                },
             }
         }
     }
 }
 
 pub fn initialize() {
+    // Initialization code remains the same
 }
