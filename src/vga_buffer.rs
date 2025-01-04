43e813867a527177ab4b3de900583a5d6ce29d73
isdood <calebjdt@gmail.com>
2025-01-04 17:44:32 -0500

    dbg.

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -173,14 +173,3 @@ pub fn clear_screen() {
         writer.column_position = 0;
     });
 }
-
-#[macro_export]
-macro_rules! print {
-    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
-}
-
-#[macro_export]
-macro_rules! println {
-    () => ($crate::print!("\n"));
-    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
-}
