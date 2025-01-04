19c8ada810a7be13ffea236ba9c2f0a9149b46e7
isdood <calebjdt@gmail.com>
2025-01-04 18:24:56 -0500

    dbg.

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,4 +1,72 @@
+use volatile::Volatile;
+use core::fmt::{self, Write};
+use spin::Mutex;
+use lazy_static::lazy_static;
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+#[repr(transparent)]
+struct Buffer {
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
+}
+
 impl Writer {
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+
     pub fn write_byte(&mut self, byte: u8) {
         match byte {
             b'\n' => self.new_line(),
@@ -7,15 +75,6 @@ impl Writer {
                     self.new_line();
                 }
 
-                // Move existing text up and write at bottom
-                for row in 1..BUFFER_HEIGHT {
-                    for col in 0..BUFFER_WIDTH {
-                        let character = self.buffer.chars[row][col].read();
-                        self.buffer.chars[row - 1][col].write(character);
-                    }
-                }
-
-                // Always write at the bottom row
                 let row = BUFFER_HEIGHT - 1;
                 let col = self.column_position;
 
@@ -24,36 +83,33 @@ impl Writer {
                     color_code: self.color_code,
                 };
 
-                self.buffer.chars[row][col].write(colored_char);
+                // Move everything up one line
+                for row in 1..BUFFER_HEIGHT {
+                    for col in 0..BUFFER_WIDTH {
+                        let character = self.buffer.chars[row][col].read();
+                        self.buffer.chars[row - 1][col].write(character);
+                    }
+                }
+
+                // Write at bottom
+                self.buffer.chars[BUFFER_HEIGHT - 1][col].write(colored_char);
                 self.column_position += 1;
             }
         }
     }
 
     fn new_line(&mut self) {
-        // Move everything up one line
         for row in 1..BUFFER_HEIGHT {
             for col in 0..BUFFER_WIDTH {
                 let character = self.buffer.chars[row][col].read();
                 self.buffer.chars[row - 1][col].write(character);
             }
         }
-        // Clear the bottom line
         self.clear_row(BUFFER_HEIGHT - 1);
-        // Reset column position but stay on bottom row
         self.column_position = 0;
     }
 
     pub fn write_string(&mut self, s: &str) {
-        // Move to bottom row first
-        self.column_position = 0;
-        for row in 1..BUFFER_HEIGHT {
-            for col in 0..BUFFER_WIDTH {
-                let character = self.buffer.chars[row][col].read();
-                self.buffer.chars[row - 1][col].write(character);
-            }
-        }
-        // Write the string
         for byte in s.bytes() {
             match byte {
                 0x20..=0x7e | b'\n' => self.write_byte(byte),
@@ -61,31 +117,61 @@ impl Writer {
             }
         }
     }
+
+    pub fn change_color(&mut self, foreground: Color, background: Color) {
+        self.color_code = ColorCode::new(foreground, background);
+    }
+
+    pub fn backspace(&mut self) {
+        if self.column_position > 0 {
+            self.column_position -= 1;
+            let row = BUFFER_HEIGHT - 1;
+            let col = self.column_position;
+
+            self.buffer.chars[row][col].write(ScreenChar {
+                ascii_character: b' ',
+                color_code: self.color_code,
+            });
+        }
+    }
 }
 
-// Modify the WRITER initialization
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
 lazy_static! {
-    pub static ref WRITER: Mutex<Writer> = Mutex::new({
-        let mut writer = Writer {
-            column_position: 0,
-            color_code: ColorCode::new(Color::White, Color::Black),
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::White, Color::Black),
                                                       buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-        };
-        // Clear screen and set position to bottom
-        for row in 0..BUFFER_HEIGHT {
-            writer.clear_row(row);
-        }
-        writer
     });
 }
 
-#[doc(hidden)]
 pub fn _print(args: fmt::Arguments) {
     use x86_64::instructions::interrupts;
     interrupts::without_interrupts(|| {
+        WRITER.lock().write_fmt(args).unwrap();
+    });
+}
+
+pub fn set_color(foreground: Color, background: Color) {
+    use x86_64::instructions::interrupts;
+    interrupts::without_interrupts(|| {
+        WRITER.lock().change_color(foreground, background);
+    });
+}
+
+pub fn clear_screen() {
+    use x86_64::instructions::interrupts;
+    interrupts::without_interrupts(|| {
         let mut writer = WRITER.lock();
-        // Ensure we're at the bottom row
+        for row in 0..BUFFER_HEIGHT {
+            writer.clear_row(row);
+        }
         writer.column_position = 0;
-        writer.write_fmt(args).unwrap();
     });
 }
