eb06620e877341372773328a986be654d8832fea
isdood <calebjdt@gmail.com>
2025-01-04 17:08:41 -0500

    dbg.

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,10 +1,10 @@
+use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, KeyCode, ScancodeSet1};
 use crate::print;
 use crate::vga_buffer::WRITER;
-use pc_keyboard::{DecodedKey, KeyCode};
 use spin::Mutex;
 use lazy_static::lazy_static;
 
-lazy_static::lazy_static! {
+lazy_static! {
     static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
     Mutex::new(Keyboard::new(layouts::Us104Key, ScancodeSet1,
                              HandleControl::Ignore)
@@ -13,10 +13,8 @@ lazy_static::lazy_static! {
 
 pub fn add_scancode(scancode: u8) {
     let mut keyboard = KEYBOARD.lock();
-
     if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
         if let Some(key) = keyboard.process_keyevent(key_event) {
-            // Remove the color change here
             match key {
                 DecodedKey::Unicode(character) => {
                     if character == '\u{0008}' {  // Backspace character
@@ -37,8 +35,4 @@ pub fn add_scancode(scancode: u8) {
             }
         }
     }
-}
-
-pub fn initialize() {
-    // No special initialization needed
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -6,7 +6,55 @@ use lazy_static::lazy_static;
 const BUFFER_HEIGHT: usize = 25;
 const BUFFER_WIDTH: usize = 80;
 
-// ... (keep Color enum and other type definitions) ...
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+#[repr(transparent)]
+struct Buffer {
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
+}
 
 impl Writer {
     fn clear_row(&mut self, row: usize) {
@@ -68,20 +116,14 @@ impl Writer {
     pub fn backspace(&mut self) {
         if self.column_position > 0 {
             self.column_position -= 1;
-            let color_code = self.color_code;
-            let col = self.column_position;
             let row = BUFFER_HEIGHT - 1;
+            let col = self.column_position;
 
             self.buffer.chars[row][col].write(ScreenChar {
                 ascii_character: b' ',
-                color_code,
+                color_code: self.color_code,
             });
         }
-    }
-
-    fn init_cursor_position(&mut self) {
-        self.column_position = 0;
-        self.clear_row(BUFFER_HEIGHT - 1);
     }
 }
 
@@ -93,14 +135,10 @@ impl fmt::Write for Writer {
 }
 
 lazy_static! {
-    pub static ref WRITER: Mutex<Writer> = Mutex::new({
-        let mut writer = Writer {
-            column_position: 0,
-            color_code: ColorCode::new(Color::White, Color::Black),
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::White, Color::Black),
                                                       buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-        };
-        writer.init_cursor_position();
-        writer
     });
 }
 
