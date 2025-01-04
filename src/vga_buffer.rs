cb61beb09305d2df560fc29f5fd58ad561e64c2d
isdood <calebjdt@gmail.com>
2025-01-04 18:42:38 -0500

    dbg.

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -76,11 +76,25 @@ impl Writer {
                     self.new_line();
                 }
 
+                // Move everything up before writing at bottom
+                if self.row_position == BUFFER_HEIGHT - 1 {
+                    // Move all lines up by one
+                    for row in 1..BUFFER_HEIGHT {
+                        for col in 0..BUFFER_WIDTH {
+                            let character = self.buffer.chars[row][col].read();
+                            self.buffer.chars[row - 1][col].write(character);
+                        }
+                    }
+                    // Clear the bottom line before writing
+                    self.clear_row(BUFFER_HEIGHT - 1);
+                }
+
                 let colored_char = ScreenChar {
                     ascii_character: byte,
                     color_code: self.color_code,
                 };
 
+                // Write at current position
                 self.buffer.chars[self.row_position][self.column_position].write(colored_char);
                 self.column_position += 1;
             }
@@ -91,12 +105,14 @@ impl Writer {
         if self.row_position < BUFFER_HEIGHT - 1 {
             self.row_position += 1;
         } else {
+            // Move everything up
             for row in 1..BUFFER_HEIGHT {
                 for col in 0..BUFFER_WIDTH {
                     let character = self.buffer.chars[row][col].read();
                     self.buffer.chars[row - 1][col].write(character);
                 }
             }
+            // Clear the new line
             self.clear_row(BUFFER_HEIGHT - 1);
         }
         self.column_position = 0;
@@ -105,9 +121,22 @@ impl Writer {
     pub fn move_to_bottom(&mut self) {
         self.row_position = BUFFER_HEIGHT - 1;
         self.column_position = 0;
+        // Clear the bottom line
+        self.clear_row(self.row_position);
     }
 
     pub fn write_string(&mut self, s: &str) {
+        // Move text up before writing if at bottom
+        if self.row_position == BUFFER_HEIGHT - 1 {
+            for row in 1..BUFFER_HEIGHT {
+                for col in 0..BUFFER_WIDTH {
+                    let character = self.buffer.chars[row][col].read();
+                    self.buffer.chars[row - 1][col].write(character);
+                }
+            }
+            self.clear_row(BUFFER_HEIGHT - 1);
+        }
+
         for byte in s.bytes() {
             match byte {
                 0x20..=0x7e | b'\n' => self.write_byte(byte),
@@ -146,6 +175,10 @@ lazy_static! {
             color_code: ColorCode::new(Color::White, Color::Black),
                                                       buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
         };
+        // Clear the screen and move to bottom
+        for row in 0..BUFFER_HEIGHT {
+            writer.clear_row(row);
+        }
         writer.move_to_bottom();
         writer
     });
@@ -156,7 +189,7 @@ pub fn _print(args: fmt::Arguments) {
     use x86_64::instructions::interrupts;
     interrupts::without_interrupts(|| {
         let mut writer = WRITER.lock();
-        writer.move_to_bottom();
+        writer.move_to_bottom();  // Move to bottom before printing
         writer.write_fmt(args).unwrap();
     });
 }
@@ -176,5 +209,13 @@ pub fn clear_screen() {
             writer.clear_row(row);
         }
         writer.move_to_bottom();
+    });
+}
+
+// Add this to handle the cursor position
+pub fn move_cursor_to_bottom() {
+    use x86_64::instructions::interrupts;
+    interrupts::without_interrupts(|| {
+        WRITER.lock().move_to_bottom();
     });
 }
