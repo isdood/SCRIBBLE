d2eb70b9ae64c74d4895e33087b1c1637be3776f
isdood <calebjdt@gmail.com>
2025-01-05 17:34:24 -0500

    dbg.

--- src/main.rs
+++ src/main.rs
@@ -3,38 +3,27 @@
 #![no_main]
 
 use core::panic::PanicInfo;
-use scribble::{print, println};
+use scribble::{println, print};
 use bootloader::{BootInfo, entry_point};
 
 entry_point!(kernel_main);
 
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("Kernel panic: {}", info);
-    scribble::hlt_loop();
-}
-
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use scribble::{print, system_println};
-
-    system_println!("Booting Scribble OS...");
-    system_println!("Starting kernel initialization...");
+    println!("Booting Scribble OS...");
+    println!("Starting kernel initialization...");
 
     // Initialize kernel components
     scribble::init_kernel(boot_info);
 
-    system_println!("Kernel initialization complete");
-    system_println!("Initializing VGA...");
+    println!("Kernel initialization complete");
+    println!("Welcome to Scribble OS");
+    print!("> ");
 
-    // Initialize VGA after kernel is ready
-    scribble::init_vga();
+    scribble::hlt_loop();
+}
 
-    system_println!("Welcome to Scribble OS");
-    print!("> ");  // This will be green because it's a prompt
-
-    // Enable interrupts
-    x86_64::instructions::interrupts::enable();
-
-    // Enter main loop
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("Kernel panic: {}", info);
     scribble::hlt_loop();
 }
