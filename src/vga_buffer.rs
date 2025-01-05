14e0d95a091e3301fb0bf66f85ddbe2f7cb56dfc
isdood <calebjdt@gmail.com>
2025-01-05 16:11:41 -0500

    dbg.

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -2,41 +2,12 @@ use volatile::Volatile;
 use core::fmt;
 use spin::Mutex;
 use lazy_static::lazy_static;
+use core::ops::{Deref, DerefMut};
 
 pub const BUFFER_HEIGHT: usize = 25;
 pub const BUFFER_WIDTH: usize = 80;
 
-#[allow(dead_code)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u8)]
-pub enum Color {
-    Black = 0,
-    Blue = 1,
-    Green = 2,
-    Cyan = 3,
-    Red = 4,
-    Magenta = 5,
-    Brown = 6,
-    LightGray = 7,
-    DarkGray = 8,
-    LightBlue = 9,
-    LightGreen = 10,
-    LightCyan = 11,
-    LightRed = 12,
-    Pink = 13,
-    Yellow = 14,
-    White = 15,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(transparent)]
-pub struct ColorCode(u8);
-
-impl ColorCode {
-    pub const fn new(foreground: Color, background: Color) -> ColorCode {
-        ColorCode((background as u8) << 4 | (foreground as u8))
-    }
-}
+// ... (Color enum and ColorCode struct remain the same)
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C)]
@@ -55,6 +26,21 @@ pub struct Writer {
     row_position: usize,
     color_code: ColorCode,
     buffer: &'static mut Buffer,
+}
+
+trait VolatileAccess<T> {
+    unsafe fn read_volatile(&self) -> T;
+    unsafe fn write_volatile(&mut self, value: T);
+}
+
+impl VolatileAccess<ScreenChar> for Volatile<ScreenChar> {
+    unsafe fn read_volatile(&self) -> ScreenChar {
+        core::ptr::read_volatile(self as *const Volatile<ScreenChar> as *const ScreenChar)
+    }
+
+    unsafe fn write_volatile(&mut self, value: ScreenChar) {
+        core::ptr::write_volatile(self as *mut Volatile<ScreenChar> as *mut ScreenChar, value)
+    }
 }
 
 impl Writer {
@@ -125,6 +111,18 @@ impl Writer {
         }
     }
 
+    pub fn get_row_position(&self) -> usize {
+        self.row_position
+    }
+
+    pub fn set_prompt_row(&mut self, row: usize) {
+        if row < BUFFER_HEIGHT {
+            self.row_position = row;
+            self.column_position = 0;
+            self.move_cursor();
+        }
+    }
+
     pub fn clear_row(&mut self, row: usize) {
         let blank = ScreenChar {
             ascii_character: b' ',
@@ -156,34 +154,6 @@ impl Writer {
             self.move_cursor();
         }
     }
-
-    pub fn get_row_position(&self) -> usize {
-        self.row_position
-    }
-
-    pub fn set_prompt_row(&mut self, row: usize) {
-        if row < BUFFER_HEIGHT {
-            self.row_position = row;
-            self.column_position = 0;
-            self.move_cursor();
-        }
-    }
-}
-
-// Keep the trait and implementation
-trait VolatileAccess<T> {
-    unsafe fn read_volatile(&self) -> T;
-    unsafe fn write_volatile(&mut self, value: T);
-}
-
-impl VolatileAccess<ScreenChar> for Volatile<ScreenChar> {
-    unsafe fn read_volatile(&self) -> ScreenChar {
-        core::ptr::read_volatile(self as *const Volatile<ScreenChar> as *const ScreenChar)
-    }
-
-    unsafe fn write_volatile(&mut self, value: ScreenChar) {
-        core::ptr::write_volatile(self as *mut Volatile<ScreenChar> as *mut ScreenChar, value)
-    }
 }
 
 impl fmt::Write for Writer {
@@ -202,24 +172,21 @@ lazy_static! {
     });
 }
 
-// Update the public functions to use WriterGuard
 pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
     use x86_64::instructions::interrupts;
 
     interrupts::without_interrupts(|| {
-        let mut writer = WriterGuard(WRITER.lock());
-        writer.write_fmt(args).unwrap();
+        WRITER.lock().write_fmt(args).unwrap();
     });
 }
 
 pub fn backspace() {
     use x86_64::instructions::interrupts;
     interrupts::without_interrupts(|| {
-        WriterGuard(WRITER.lock()).backspace();
+        WRITER.lock().backspace();
     });
 }
-
 
 pub fn clear_screen() {
     use x86_64::instructions::interrupts;
