a342a85301dd73532ca650206c699205eb3d1b97
isdood <calebjdt@gmail.com>
2025-01-05 13:05:53 -0500

    dbg.

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -13,25 +13,24 @@ pub enum InterruptIndex {
     Keyboard,
 }
 
-lazy_static::lazy_static! {
-    static ref IDT: InterruptDescriptorTable = {
-        let mut idt = InterruptDescriptorTable::new();
-        idt.breakpoint.set_handler_fn(breakpoint_handler);
-        idt.page_fault.set_handler_fn(page_fault_handler);
-        unsafe {
-            idt.double_fault
-            .set_handler_fn(double_fault_handler)
-            .set_stack_index(crate::gdt::DOUBLE_FAULT_IST_INDEX);
-        }
-        idt.general_protection_fault.set_handler_fn(general_protection_fault_handler);
-        idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
-        idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
-        idt
-    };
+impl InterruptIndex {
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
+
+    fn as_usize(self) -> usize {
+        self.as_u8() as usize
+    }
 }
 
 pub static PICS: spin::Mutex<ChainedPics> =
 spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+
+extern "x86-interrupt" fn breakpoint_handler(
+    stack_frame: InterruptStackFrame
+) {
+    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
 
 extern "x86-interrupt" fn page_fault_handler(
     stack_frame: InterruptStackFrame,
@@ -64,4 +63,47 @@ extern "x86-interrupt" fn double_fault_handler(
     crate::hlt_loop();
 }
 
-// ... rest of the handlers remain the same ...
+extern "x86-interrupt" fn timer_interrupt_handler(
+    _stack_frame: InterruptStackFrame
+) {
+    unsafe {
+        PICS.lock()
+        .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+    }
+}
+
+extern "x86-interrupt" fn keyboard_interrupt_handler(
+    _stack_frame: InterruptStackFrame
+) {
+    use x86_64::instructions::port::Port;
+
+    let mut port = Port::new(0x60);
+    let scancode: u8 = unsafe { port.read() };
+    crate::keyboard::handle_scancode(scancode);
+
+    unsafe {
+        PICS.lock()
+        .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
+    }
+}
+
+lazy_static::lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
+        unsafe {
+            idt.double_fault
+            .set_handler_fn(double_fault_handler)
+            .set_stack_index(crate::gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+        idt.general_protection_fault.set_handler_fn(general_protection_fault_handler);
+        idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
+        idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
+        idt
+    };
+}
+
+pub fn init_idt() {
+    IDT.load();
+}
