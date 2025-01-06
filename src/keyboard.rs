a6c2755fef5cff982586b37746cdc168a882e5df
isdood <calebjdt@gmail.com>
2025-01-06 18:52:13 -0500

    dbg.

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,13 +1,14 @@
-         //IMPORTS\\
+// In keyboard.rs
+//IMPORTS\\
 /////////////////////////////////
-
 use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
 use spin::Mutex;
 use lazy_static::lazy_static;
 use x86_64::structures::idt::InterruptStackFrame;
 use crate::interrupts::{PICS, InterruptIndex};
-
-//////////// END //////////////
+use crate::print;
+use crate::println;
+use crate::vga_buffer::CursorMode;  // Add this import
 
 lazy_static! {
     static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
@@ -18,47 +19,65 @@ pub extern "x86-interrupt" fn keyboard_interrupt_handler(
     _stack_frame: InterruptStackFrame
 ) {
     use x86_64::instructions::port::Port;
-    use x86_64::instructions::interrupts;
 
-    interrupts::without_interrupts(|| {
-        let mut keyboard = KEYBOARD.lock();
-        let mut port = Port::new(0x60);
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60);
 
-        let scancode: u8 = unsafe { port.read() };
+    let scancode: u8 = unsafe { port.read() };
 
-        if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
-            if let Some(key) = keyboard.process_keyevent(key_event) {
-                match key {
-                    DecodedKey::Unicode(character) => {
-                        let mut writer = crate::vga_buffer::WRITER.lock();
-                        let should_handle = {
-                            let next_pos = if character == '\u{8}' && writer.column_position > 0 {
-                                writer.column_position - 1
-                            } else {
-                                writer.column_position
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => {
+                    // Handle cursor mode switching
+                    match character {
+                        // Ctrl+H for Hardware cursor (you'll need to press H)
+                        'H' => {
+                            crate::vga_buffer::switch_cursor_mode(CursorMode::Hardware);
+                            println!("Switched to hardware cursor");
+                        },
+                        // Ctrl+S for Software cursor (you'll need to press S)
+                        'S' => {
+                            crate::vga_buffer::switch_cursor_mode(CursorMode::Software);
+                            println!("Switched to software cursor");
+                        },
+                        // Normal character handling
+                        _ => {
+                            let should_handle = {
+                                let writer = crate::vga_buffer::WRITER.lock();
+                                let next_pos = if character == '\u{8}' && writer.column_position > 0 {
+                                    writer.column_position - 1
+                                } else if writer.needs_wrap() {
+                                    0 // Allow wrapping to next line
+                                } else {
+                                    writer.column_position
+                                };
+                                !writer.protected_region.contains(
+                                    if writer.needs_wrap() { writer.row_position + 1 } else { writer.row_position },
+                                        next_pos
+                                )
                             };
-                            !writer.protected_region.contains(writer.row_position, next_pos)
-                        };
 
-                        if should_handle {
-                            match character {
-                                '\u{8}' => writer.backspace(),
-                                   '\n' => {
-                                       writer.write_byte(b'\n');
-                                       writer.write_prompt();
-                                   },
-                                   _ => writer.write_byte(character as u8),
+                            if should_handle {
+                                match character {
+                                    '\u{8}' => crate::vga_buffer::backspace(),
+                                    '\n' => {
+                                        print!("{}", character);
+                                        crate::vga_buffer::write_prompt();
+                                    },
+                                    _ => print!("{}", character),
+                                }
                             }
                         }
-                    },
-                    DecodedKey::RawKey(_) => (), // Ignore raw keys
-                }
+                    }
+                },
+                DecodedKey::RawKey(key) => print!("{:?}", key),
             }
         }
+    }
 
-        unsafe {
-            PICS.lock()
-            .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
-        }
-    });
+    unsafe {
+        PICS.lock()
+        .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
+    }
 }
