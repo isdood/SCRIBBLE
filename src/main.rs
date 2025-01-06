5f962b929ef77361890175b6be08f19261ccea89
isdood <calebjdt@gmail.com>
2025-01-06 10:08:56 -0500

    dbg.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use scribble::{println, vga_buffer}; // Add vga_buffer to the imports
+use scribble::println; // Add vga_buffer to the imports
 
 entry_point!(kernel_main);
 
