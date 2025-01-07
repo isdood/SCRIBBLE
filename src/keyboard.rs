b5d66561f052a372ba9bf411c3361d08c3de61d8
isdood <calebjdt@gmail.com>
2025-01-06 20:28:54 -0500

    dbg.

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,29 +1,8 @@
-// In keyboard.rs
-//IMPORTS\\
-/////////////////////////////////
-use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
-use spin::Mutex;
-use lazy_static::lazy_static;
-use x86_64::structures::idt::InterruptStackFrame;
-use crate::interrupts::{PICS, InterruptIndex};
-use crate::print;
-use crate::println;
-use crate::vga_buffer::CursorMode;  // Add this import
-
-lazy_static! {
-    static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
-    Mutex::new(Keyboard::new(ScancodeSet1::new(), layouts::Us104Key, HandleControl::Ignore));
-}
-
-pub extern "x86-interrupt" fn keyboard_interrupt_handler(
-    _stack_frame: InterruptStackFrame
-) {
-    use x86_64::instructions::port::Port;
-
+// In src/keyboard.rs
+pub extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
     let mut keyboard = KEYBOARD.lock();
     let mut port = Port::new(0x60);
-
-    let scancode: u8 = unsafe { port.read() };
+    let scancode: u8 = port.read(); // Port read is safe in this context as it's the keyboard data port
 
     if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
         if let Some(key) = keyboard.process_keyevent(key_event) {
@@ -75,5 +54,10 @@ pub extern "x86-interrupt" fn keyboard_interrupt_handler(
                     }
                 },
                 DecodedKey::RawKey(key) => print!("{:?}", key),
-}
+            }
+        }
+    }
 
+    PICS.lock()
+    .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
+}
