4c02a7ad03140c4d6d51c01b839fb8423c3be445
isdood <calebjdt@gmail.com>
2025-01-08 03:44:54 -0500

    dbg.

--- bootloader/build.sh
+++ bootloader/build.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 set -e
 
 # Clean previous builds
@@ -11,19 +10,17 @@ cargo +nightly build \
     -Z build-std=core,compiler_builtins \
     -Z build-std-features=compiler-builtins-mem
 
-# Check binary size
+# Get bootloader size
 bootloader_size=$(wc -c < target/x86_64-scribble/release/spinUP)
-if [ "$bootloader_size" -gt 512 ]; then
-    echo "Error: Bootloader size ($bootloader_size bytes) exceeds 512 bytes!"
-    exit 1
-fi
+echo "Second-stage bootloader size: $bootloader_size bytes"
 
-# Create disk image
+# Calculate number of sectors needed (512 bytes per sector)
+sectors=$(( ($bootloader_size + 511) / 512 ))
+
+# Create disk image (2880 sectors = 1.44MB floppy)
 dd if=/dev/zero of=disk.img bs=512 count=2880
-dd if=target/x86_64-scribble/release/spinUP of=disk.img conv=notrunc bs=512 count=1
+dd if=target/x86_64-scribble/release/spinUP of=disk.img conv=notrunc bs=512 seek=1 count=$sectors
 
-# Show the first bytes of the disk image
+echo "Bootloader written to disk image ($sectors sectors)"
 echo "Bootloader hexdump:"
 hexdump -C disk.img | head -n 8
-
-echo "Bootloader size: $bootloader_size bytes"
