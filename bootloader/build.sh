265f86758b8ab1bc7cdb416af2e4ef89322e9cbc
isdood <calebjdt@gmail.com>
2025-01-08 01:11:22 -0500

    dbg.

--- bootloader/build.sh
+++ bootloader/build.sh
@@ -11,6 +11,13 @@ cargo +nightly build \
     -Z build-std=core,compiler_builtins \
     -Z build-std-features=compiler-builtins-mem
 
+# Check binary size
+bootloader_size=$(wc -c < target/x86_64-scribble/release/spinUP)
+if [ "$bootloader_size" -gt 512 ]; then
+    echo "Error: Bootloader size ($bootloader_size bytes) exceeds 512 bytes!"
+    exit 1
+fi
+
 # Create disk image
 dd if=/dev/zero of=disk.img bs=512 count=2880
 dd if=target/x86_64-scribble/release/spinUP of=disk.img conv=notrunc bs=512 count=1
@@ -18,3 +25,5 @@ dd if=target/x86_64-scribble/release/spinUP of=disk.img conv=notrunc bs=512 coun
 # Show the first bytes of the disk image
 echo "Bootloader hexdump:"
 hexdump -C disk.img | head -n 8
+
+echo "Bootloader size: $bootloader_size bytes"
--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -3,17 +3,38 @@
 
 use core::panic::PanicInfo;
 
-#[no_mangle]
-#[link_section = ".text._start"]
-pub extern "C" fn _start() -> ! {
-    unsafe {
-        core::ptr::write_volatile(0xb8000 as *mut u8, b'X');
-        core::ptr::write_volatile(0xb8001 as *mut u8, 0x0F);
-    }
-    loop {}
-}
+// Define our assembly code using global_asm
+core::arch::global_asm!(
+    // Real mode code
+    ".code16",
+    ".section .boot,\"ax\"",
+    ".global _start",
+    "_start:",
+    "    cli",                // Disable interrupts
+    "    xor ax, ax",        // Zero ax
+    "    mov ds, ax",        // Set up data segment
+    "    mov es, ax",        // Set up extra segment
+    "    mov ss, ax",        // Set up stack segment
+    "    mov sp, 0x7C00",    // Set up stack pointer
+    "    cld",               // Clear direction flag
+
+    // Print character
+    "    mov ah, 0x0E",      // BIOS teletype output
+    "    mov al, 'H'",       // Character to print
+    "    mov bx, 0x0000",    // Page 0, black on black
+    "    int 0x10",          // Call BIOS video interrupt
+
+    // Infinite loop
+    "1:",
+    "    hlt",              // Halt the CPU
+    "    jmp 1b",           // Jump back to local label 1 if interrupted
+
+    // Ensure we don't overflow the boot sector
+    ".org 510 - (. - _start)",
+                        ".byte 0x55, 0xAA",     // Boot signature inline
+);
 
 #[panic_handler]
-fn panic(_: &PanicInfo) -> ! {
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
