c5e3123e6a0c7cc6c38aec98e08a4f0c5d64d1ff
isdood <calebjdt@gmail.com>
2025-01-11 14:02:02 -0500

    dbg.

--- spinUP/gear1/src/main.rs
+++ spinUP/gear1/src/main.rs
@@ -1,77 +1,55 @@
-#![no_std]  // Don't link the Rust standard library
-#![no_main] // Disable all Rust-level entry points
+#![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
-// The panic handler
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(_: &PanicInfo) -> ! {
     loop {}
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn _start() -> ! {
-    // Setup stack
     core::arch::asm!(
-        "mov sp, 0x7c00",
-        options(nomem, nostack)
-    );
-
-    // Reset segments
-    core::arch::asm!(
+        // Setup segments and stack
         "xor ax, ax",
         "mov ds, ax",
         "mov es, ax",
         "mov ss, ax",
+        "mov sp, 0x7c00",
+
+        // Build disk packet on stack
+        "push word ptr 0",      // Upper LBA bits
+        "push word ptr 0",
+        "push word ptr 0",      // Lower LBA bits
+        "push word ptr 1",      // Starting at sector 1
+        "push word ptr 0x07e0", // Buffer segment
+        "push word ptr 0",      // Buffer offset
+        "push word ptr 63",     // Sector count
+        "push word ptr 16",     // Packet size
+
+        // Read disk
+        "mov si, sp",          // Packet pointer
+        "mov ah, 0x42",        // Extended read
+        "mov dl, 0x80",        // First hard drive
+        "int 0x13",
+        "jc 1f",              // On error, halt
+
+        // Jump to Gear2
+        "add sp, 16",          // Clean stack
+        "push word ptr 0",     // CS = 0
+        "push word ptr 0x7e00", // IP = 0x7e00
+        "retf",                // Far jump
+
+        // Error handler
+        "1: cli",
+        "hlt",
         options(nomem, nostack)
-    );
-
-    // Load Gear2
-    let drive: u8 = 0x80;  // Boot drive
-
-    // Set up disk address packet on stack
-    core::arch::asm!(
-        // Create disk address packet
-        "push word ptr 0",     // Upper 16 bits of LBA (48-bit)
-    "push word ptr 0",     // Upper 32 bits of LBA (48-bit)
-    "push word ptr 0",     // Lower 32 bits of LBA - high word
-    "push word ptr 1",     // Lower 32 bits of LBA - low word (sector 1)
-    "push word ptr 0x07e0", // Segment of buffer
-    "push word ptr 0",     // Offset of buffer
-    "push word ptr 63",    // Number of sectors (63 more sectors)
-    "push word ptr 16",    // Size of packet (16 bytes)
-    "mov si, sp",         // SI points to packet
-
-    // Call INT 13h
-    "mov ah, 0x42",      // Extended read
-    "mov dl, {drive}",   // Drive number
-    "int 0x13",          // Call BIOS
-    "jc 2f",            // If error, jump to error handler
-
-    // Clean up stack
-    "add sp, 16",       // Remove packet from stack
-
-    // Jump to Gear2
-    "push word ptr 0",   // CS = 0
-    "push word ptr 0x7e00", // IP = 0x7e00
-    "retf",              // Far return to Gear2
-
-    // Error handler
-    "2:",
-    "mov al, 'E'",      // Error character
-    "mov ah, 0x0e",     // TTY output
-    "int 0x10",         // Print it
-    "cli",              // Disable interrupts
-    "hlt",              // Halt
-    drive = in(reg_byte) drive,
-                     options(nomem)
     );
 
     loop {}
 }
 
-// Ensure the binary has a 512-byte size and ends with the boot signature
-#[used]
+#[link_section = ".signature"]
 #[no_mangle]
-#[link_section = ".boot_signature"]
-static BOOT_SIGNATURE: [u8; 2] = [0x55, 0xaa];
+pub static BOOT_SIGNATURE: [u8; 2] = [0x55, 0xaa];
