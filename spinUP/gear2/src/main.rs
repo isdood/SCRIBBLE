68b6f5fdb700b2ad2072110ae8d52b9217c455bc
isdood <calebjdt@gmail.com>
2025-01-11 14:18:36 -0500

    dbg.

--- spinUP/gear2/src/main.rs
+++ spinUP/gear2/src/main.rs
@@ -498,27 +498,17 @@ pub unsafe extern "C" fn _start() -> ! {
     // Far jump to 64-bit code
     core::arch::asm!(
         ".code32",
-        "ljmp $8,$2f",           // CS selector 8 (code)
-    ".align 8",
-    "2:",
-    ".code64",
-    "mov rsp, {stack}",
-    "jmp {target}",
-    stack = const 0x7c00,
-    target = sym rust_main,
-    options(noreturn)
+        "pushd $8",           // Push code segment selector
+        "lea eax, [2f]",      // Get address of label
+        "push eax",           // Push target address
+        "retf",               // Far return to switch to 64-bit mode
+        ".align 8",
+        "2:",
+        ".code64",
+        "mov rsp, {stack}",   // Set up 64-bit stack
+        "jmp {target}",       // Jump to rust_main
+        stack = const 0x7c00,
+        target = sym rust_main,
+        options(noreturn)
     );
-}
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {
-        unsafe {
-            core::arch::asm!(
-                ".code32",
-                "hlt",
-                options(nomem, nostack)
-            );
-        }
-    }
 }
