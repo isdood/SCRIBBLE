34945bd636b824b4bec77d477c516f47981c9429
isdood <calebjdt@gmail.com>
2025-01-11 15:55:32 -0500

    dbg.

--- spinUP/gear1/src/main.rs
+++ spinUP/gear1/src/main.rs
@@ -16,34 +16,38 @@ pub extern "C" fn _start() -> ! {
             "mov sp, 0x7C00",
 
             // Save boot drive
-            "mov [0x7E00], dl",
+            "mov byte ptr [0x7E00], dl",
 
             // Enable A20
             "in al, 0x92",
             "or al, 2",
             "out 0x92, al",
 
-            // Setup DAP on stack
-            "push dword ptr 0", // _pad2
-            "push dword ptr 1", // LBA = 1
-            "push dword ptr 0x07E0_0000", // seg:off
-            "push word ptr 16", // count = 16 sectors
-            "push word ptr 0x10", // sz = 16, _pad = 0
-            "mov si, sp", // SI points to DAP
+            // Build DAP on stack
+            "push word ptr 0",         // Upper _pad2
+            "push word ptr 0",         // Lower _pad2
+            "push word ptr 0",         // Upper LBA
+            "push word ptr 1",         // Lower LBA = 1
+            "push word ptr 0x07E0",    // segment
+            "push word ptr 0",         // offset
+            "push word ptr 16",        // count
+            "push word ptr 0x10",      // size=16
+            "mov si, sp",              // SI points to DAP
 
             // Load sectors
             "mov ah, 0x42",
             "int 0x13",
-            "add sp, 16", // Clean stack
-            "jc 2f",
+            "add sp, 16",              // Clean stack
+            "jc 2f",                   // Jump if error
 
             // Jump to gear2
-            "push word ptr 0x07E0",
-            "push word ptr 0",
-            "retf",
+            "push word ptr 0x07E0",    // segment
+            "push word ptr 0",         // offset
+            "retf",                    // Far return
 
             // Error: print 'E' and halt
-            "2: mov ax, 0x0E45",
+            "2:",
+            "mov ax, 0x0E45",
             "int 0x10",
             "cli",
             "hlt",
