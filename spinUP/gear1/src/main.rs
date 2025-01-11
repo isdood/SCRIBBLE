f4c0b1f56661216f215288c9dd8c325254ebcaee
isdood <calebjdt@gmail.com>
2025-01-11 14:04:26 -0500

    dbg.

--- spinUP/gear1/src/main.rs
+++ spinUP/gear1/src/main.rs
@@ -1,16 +1,14 @@
 #![no_std]
 #![no_main]
-#![no_mangle]
+
+use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_: &PanicInfo) -> ! { loop {} }
 
-use core::panic::PanicInfo;
-
 #[no_mangle]
 #[link_section = ".start"]
-#[export_name = "_start"]
-pub unsafe extern "C" fn _real_start() -> ! {
+pub unsafe extern "C" fn _start() -> ! {
     core::arch::asm!(
         // Init segments
         "xor ax, ax",
@@ -19,14 +17,14 @@ pub unsafe extern "C" fn _real_start() -> ! {
         "mov sp, 0x7c00",
 
         // Packet: size=16, sectors=63, dest=0x7e00, lba=1
-        "push 0",
-        "push 0",
-        "push 0",
-        "push 1",
-        "push 0x07e0",
-        "push 0",
-        "push 63",
-        "push 16",
+        "push word ptr 0",
+        "push word ptr 0",
+        "push word ptr 0",
+        "push word ptr 1",
+        "push word ptr 0x07e0",
+        "push word ptr 0",
+        "push word ptr 63",
+        "push word ptr 16",
 
         // Read disk
         "mov si, sp",
@@ -36,7 +34,9 @@ pub unsafe extern "C" fn _real_start() -> ! {
         "jc 2f",
 
         // Jump to Gear2
-        "jmp 0:0x7e00",
+        "push word ptr 0",     // CS = 0
+        "push word ptr 0x7e00", // IP = 0x7e00
+        "retf",                // Far return
 
         "2: hlt",
         options(nomem, nostack)
@@ -45,4 +45,5 @@ pub unsafe extern "C" fn _real_start() -> ! {
 }
 
 #[link_section = ".sig"]
-pub static SIG: [u8; 2] = [0x55, 0xaa];
+#[no_mangle]
+pub static BOOT_SIGNATURE: [u8; 2] = [0x55, 0xaa];
