d4ae7b3cc0903071b2c26e5d44d2d1fb9d5c313d
isdood <calebjdt@gmail.com>
2025-01-11 16:30:34 -0500

    dbg.

--- spinUP/gear1/src/main.rs
+++ spinUP/gear1/src/main.rs
@@ -9,31 +9,45 @@ fn panic(_: &core::panic::PanicInfo) -> ! { loop {} }
 pub extern "C" fn _start() -> ! {
     unsafe {
         core::arch::asm!(
+            // Setup
             "xor ax, ax",
             "mov ds, ax",
             "mov ss, ax",
             "mov sp, 0x7C00",
             "mov byte ptr [0x7E00], dl",
+
+            // Enable A20
             "in al, 0x92",
             "or al, 2",
             "out 0x92, al",
+
+            // Load sectors using DAP
             "mov si, dap",
             "mov ah, 0x42",
             "int 0x13",
             "jc error",
+
+            // Jump to gear2
             "push word ptr 0x07E0",
             "push word ptr 0",
             "retf",
+
+            // Error handler
             "error:",
             "mov ax, 0x0E45",
             "int 0x10",
             "cli",
             "hlt",
-            dap:
-            .byte 16, 0",
-            .word 16, 0, 0x07E0",
-            .long 1, 0",
+
+            // Disk Address Packet (DAP)
+            "dap:",
+            ".byte 16",         // size of DAP
+            ".byte 0",          // unused
+            ".word 16",         // sectors to read
+            ".word 0",          // offset
+            ".word 0x07E0",     // segment
+            ".quad 1",          // LBA
             options(noreturn)
         );
     }
-    }
+}
