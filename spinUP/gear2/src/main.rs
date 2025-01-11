2f4c27a31b553c35cda3e4023ea4a5538ca345f5
isdood <calebjdt@gmail.com>
2025-01-11 15:01:20 -0500

    dbg.

--- spinUP/gear2/src/main.rs
+++ spinUP/gear2/src/main.rs
@@ -1,25 +1,34 @@
 #![no_std]
 #![no_main]
+#![feature(asm_const)]
 
-use core::panic::PanicInfo;
+// Tell the compiler we're targeting i686 initially
+#[cfg(target_arch = "x86_64")]
+global_asm!(".code32");
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+#[link_section = ".text"]
+pub unsafe extern "C" fn _start() -> ! {
+    // We start in 32-bit protected mode
     let vga = 0xb8000 as *mut u16;
-    unsafe {
-        *vga = 0x0F4F;       // White 'O' on black
-        *(vga.add(1)) = 0x0F4B; // White 'K' on black
-        *(vga.add(2)) = 0x0F21; // White '!' on black
-    }
+
+    // Write "OK!" to show we're running
+    *vga = 0x0F4F;       // White 'O' on black
+    *(vga.add(1)) = 0x0F4B; // White 'K' on black
+    *(vga.add(2)) = 0x0F21; // White '!' on black
 
     loop {
-        core::arch::asm!("hlt");
+        unsafe {
+            core::arch::asm!("hlt");
+        }
     }
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {
-        core::arch::asm!("hlt");
+        unsafe {
+            core::arch::asm!("hlt");
+        }
     }
 }
