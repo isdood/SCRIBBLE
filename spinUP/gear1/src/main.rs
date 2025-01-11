bf44e91dc7c78f4f3e49a785af8adaa587f3893f
isdood <calebjdt@gmail.com>
2025-01-11 14:05:31 -0500

    dbg.

--- spinUP/gear1/src/main.rs
+++ spinUP/gear1/src/main.rs
@@ -6,44 +6,45 @@ use core::panic::PanicInfo;
 #[panic_handler]
 fn panic(_: &PanicInfo) -> ! { loop {} }
 
+#[link_section = ".text.start"]
 #[no_mangle]
-#[link_section = ".start"]
 pub unsafe extern "C" fn _start() -> ! {
     core::arch::asm!(
-        // Init segments
-        "xor ax, ax",
-        "mov ds, ax",
-        "mov ss, ax",
-        "mov sp, 0x7c00",
-
-        // Packet: size=16, sectors=63, dest=0x7e00, lba=1
-        "push word ptr 0",
-        "push word ptr 0",
-        "push word ptr 0",
-        "push word ptr 1",
-        "push word ptr 0x07e0",
-        "push word ptr 0",
-        "push word ptr 63",
-        "push word ptr 16",
-
-        // Read disk
-        "mov si, sp",
-        "mov dl, 0x80",
-        "mov ah, 0x42",
-        "int 0x13",
-        "jc 2f",
-
-        // Jump to Gear2
-        "push word ptr 0",     // CS = 0
-        "push word ptr 0x7e00", // IP = 0x7e00
-        "retf",                // Far return
-
-        "2: hlt",
-        options(nomem, nostack)
+        ".code16",
+        // Setup
+        "xor ax,ax",
+        "mov ss,ax",
+        "mov sp,0x7c00",
+        "mov ds,ax",
+        // DAP on stack
+        "mov ax,0x10",  // Size and reserved
+        "push ax",
+        "mov ax,0x3f",  // Sectors (63)
+    "push ax",
+    "xor ax,ax",    // Offset
+    "push ax",
+    "mov ax,0x7e0", // Segment
+    "push ax",
+    "mov ax,1",     // LBA low
+    "push ax",
+    "xor ax,ax",    // LBA high
+    "push ax",
+    "push ax",
+    "push ax",
+    // Read
+    "mov ah,0x42",
+    "mov dl,0x80",
+    "mov si,sp",
+    "int 0x13",
+    "jc 2f",
+    // Jump
+    "ljmp 0,0x7e00",
+    "2:hlt",
+    options(nomem, nostack),
     );
     loop {}
 }
 
-#[link_section = ".sig"]
+#[link_section = ".boot_sig"]
 #[no_mangle]
-pub static BOOT_SIGNATURE: [u8; 2] = [0x55, 0xaa];
+pub static BOOT_SIG: [u8; 2] = [0x55, 0xaa];
