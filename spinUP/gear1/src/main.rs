882eb1f5bce942516db1dcefd131b27e9cbfb299
isdood <calebjdt@gmail.com>
2025-01-11 10:30:55 -0500

    dbg.

--- spinUP/gear1/src/main.rs
+++ spinUP/gear1/src/main.rs
@@ -4,6 +4,7 @@
 use core::panic::PanicInfo;
 
 #[repr(C, packed)]
+#[derive(Copy, Clone)]
 struct Dap {
     sz: u8,
     _pad: u8,
@@ -17,50 +18,58 @@ struct Dap {
 #[link_section = ".boot.text"]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let dap = Dap {
+        sz: 16,
+        _pad: 0,
+        cnt: 32,
+        off: 0,
+        seg: 0x07E0,
+        lba: 1,
+        _pad2: 0,
+    };
+
     unsafe {
-        // Setup segments and stack
         core::arch::asm!(
+            // Initialize segments
             "xor ax, ax",
             "mov ds, ax",
             "mov es, ax",
             "mov ss, ax",
             "mov sp, 0x7C00",
-        );
 
-        // Load gear2
-        let dap = Dap {
-            sz: 16,
-            _pad: 0,
-            cnt: 32,
-            off: 0,
-            seg: 0x07E0,
-            lba: 1,
-            _pad2: 0,
-        };
+            // Store boot drive
+            "mov [0x7E00], dl",
 
-        // Store boot drive and enable A20
-        core::arch::asm!(
-            "mov [0x7E00], dl",  // Store boot drive at known location
-            "in al, 0x92",       // Fast A20 gate
+            // Enable A20
+            "in al, 0x92",
             "or al, 2",
             "out 0x92, al",
-            "mov ah, 0x42",      // Load sectors
-            "mov si, {0:x}",
+
+            // Load sectors
+            "mov ah, 0x42",
+            "mov si, {0}",
             "int 0x13",
-            "jc 2f",            // Error if carry set
-            "push word ptr 0x07E0",
-            "push word ptr 0",
-            "retf",             // Jump to gear2
+            "jc 2f",
+
+            // Jump to gear2
+            "pushw 0x07E0",
+            "pushw 0",
+            "retf",
+
             "2:",
-            "mov ax, 0x0E45",   // Print 'E' on error
+            "mov al, 'E'",
+            "mov ah, 0x0E",
             "int 0x10",
+            "cli",
             "hlt",
             in(reg) &dap,
-                         options(noreturn),
+                         options(noreturn)
         );
     }
 }
 
 #[panic_handler]
-#[no_mangle]
-fn panic(_: &PanicInfo) -> ! { loop {} }
+#[link_section = ".boot.text"]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
