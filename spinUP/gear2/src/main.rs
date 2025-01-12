b05350e3ddd80bc557a5ba10669c76b0a64853c9
isdood <calebjdt@gmail.com>
2025-01-11 21:24:51 -0500

    dbg.

--- spinUP/gear2/src/main.rs
+++ spinUP/gear2/src/main.rs
@@ -373,50 +373,6 @@ extern "x86-interrupt" fn page_fault_handler() -> ! {
     }
 }
 
-#[naked]
-extern "x86-interrupt" fn timer_interrupt_handler() -> ! {
-    unsafe {
-        core::arch::naked_asm!(
-            "push rax",
-            "push rcx",
-            "push rdx",
-            "push rbx",
-            "push rbp",
-            "push rsi",
-            "push rdi",
-            "push r8",
-            "push r9",
-            "push r10",
-            "push r11",
-            "push r12",
-            "push r13",
-            "push r14",
-            "push r15",
-
-            "mov al, 0x20",
-            "out 0x20, al",
-
-            "pop r15",
-            "pop r14",
-            "pop r13",
-            "pop r12",
-            "pop r11",
-            "pop r10",
-            "pop r9",
-            "pop r8",
-            "pop rdi",
-            "pop rsi",
-            "pop rbp",
-            "pop rbx",
-            "pop rdx",
-            "pop rcx",
-            "pop rax",
-
-            "iretq",
-        );
-    }
-}
-
 #[no_mangle]
 pub unsafe extern "C" fn _start() -> ! {
     core::arch::asm!("cli");
