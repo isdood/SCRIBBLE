e734860aa2ae0a2cc8ddb22254619ae0974219f4
isdood <calebjdt@gmail.com>
2025-01-11 23:29:15 -0500

    dbg.

--- spinUP/gear2/src/main.rs
+++ spinUP/gear2/src/main.rs
@@ -281,8 +281,7 @@ unsafe extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: Interrupt
         "mov al, 0x20",
         "out 0x20, al",  // Send EOI to PIC
         "pop rax",
-        "iretq",
-        options(nomem, nostack)
+        "iretq"
     );
 }
 
