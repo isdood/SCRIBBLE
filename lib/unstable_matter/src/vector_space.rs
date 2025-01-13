3b282e5fc59ffa03353bbf4f9132483bd570469d
isdood <calebjdt@gmail.com>
2025-01-12 22:20:19 -0500

    dbg.

--- lib/unstable_matter/src/vector_space.rs
+++ lib/unstable_matter/src/vector_space.rs
@@ -9,19 +9,13 @@ use crate::tracked_ufo::TrackedUFO;
 use crate::vector::Vector3D;
 
 pub struct VectorSpace {
-    pub origin: usize,
-    pub mesh: SpaceTime<MeshCell>,
-    pub config: SpaceConfig,
-    pub ufo_state: TrackedUFO,
-    pub metadata: SpaceMetadata,
-    pub morph_tracker: MorphTracker,
-    pub state: UFOState,
-}
-
-impl Default for VectorSpace {
-    fn default() -> Self {
-        Self::new(0, 1024)
-    }
+    origin: usize,
+    mesh: SpaceTime<MeshCell>,
+    config: SpaceConfig,
+    ufo_state: TrackedUFO,
+    metadata: SpaceMetadata,
+    morph_tracker: MorphTracker,
+    state: UFOState,
 }
 
 impl VectorSpace {
@@ -33,14 +27,7 @@ impl VectorSpace {
         );
 
         let mesh_size = (config.cells.x * config.cells.y * config.cells.z) as usize;
-        let current_time = 1705112243; // 2025-01-13 03:17:23 UTC
-
-        let metadata = SpaceMetadata {
-            creation_time: AtomicUsize::new(current_time),
-            last_modified: AtomicUsize::new(current_time),
-            creator: "isdood",
-            last_modifier: "isdood",
-        };
+        let metadata = SpaceMetadata::new("isdood");
 
         Self {
             origin,
@@ -50,7 +37,6 @@ impl VectorSpace {
             metadata,
             morph_tracker: MorphTracker::new(),
             state: UFOState::Flying,
-            timestamp: AtomicUsize::new(current_time),
         }
     }
 
@@ -60,7 +46,7 @@ impl VectorSpace {
             let cell = MeshCell::new();
             unsafe { self.mesh.write_at(i, cell) };
         }
-        self.timestamp.store(1705112243, Ordering::SeqCst); // 2025-01-13 03:17:23 UTC
+        self.metadata.timestamp.store(1705112339, Ordering::SeqCst); // 2025-01-13 03:18:59 UTC
         Ok(())
     }
 
@@ -77,7 +63,7 @@ impl VectorSpace {
         }
         unsafe {
             self.mesh.write_at(index, cell);
-            self.timestamp.store(1705112243, Ordering::SeqCst); // 2025-01-13 03:17:23 UTC
+            self.metadata.last_modified.store(1705112339, Ordering::SeqCst); // 2025-01-13 03:18:59 UTC
         }
         Ok(())
     }
@@ -85,21 +71,21 @@ impl VectorSpace {
     pub fn land(mut self) -> Result<Self, &'static str> {
         self.ufo_state.track();
         self.state = self.state.transition_to_landed()?;
-        self.timestamp.store(1705112243, Ordering::SeqCst); // 2025-01-13 03:17:23 UTC
+        self.metadata.timestamp.store(1705112339, Ordering::SeqCst); // 2025-01-13 03:18:59 UTC
         Ok(self)
     }
 
     pub fn hover(mut self) -> Result<Self, &'static str> {
         self.ufo_state.track();
         self.state = self.state.transition_to_hovering()?;
-        self.timestamp.store(1705112243, Ordering::SeqCst); // 2025-01-13 03:17:23 UTC
+        self.metadata.timestamp.store(1705112339, Ordering::SeqCst); // 2025-01-13 03:18:59 UTC
         Ok(self)
     }
 
     pub fn take_off(mut self) -> Result<Self, &'static str> {
         self.ufo_state.track();
         self.state = self.state.transition_to_flying()?;
-        self.timestamp.store(1705112243, Ordering::SeqCst); // 2025-01-13 03:17:23 UTC
+        self.metadata.timestamp.store(1705112339, Ordering::SeqCst); // 2025-01-13 03:18:59 UTC
         Ok(self)
     }
 
@@ -109,7 +95,7 @@ impl VectorSpace {
 
     pub fn init_morph_types(&mut self) -> Result<(), &'static str> {
         self.morph_tracker.register_file_type(FileType::Rust)?;
-        self.timestamp.store(1705112243, Ordering::SeqCst); // 2025-01-13 03:17:23 UTC
+        self.metadata.timestamp.store(1705112339, Ordering::SeqCst); // 2025-01-13 03:18:59 UTC
         Ok(())
     }
 
@@ -118,10 +104,10 @@ impl VectorSpace {
     }
 
     pub fn update_metadata(&mut self) {
-        let current_time = 1705112243; // 2025-01-13 03:17:23 UTC
+        let current_time = 1705112339; // 2025-01-13 03:18:59 UTC
         self.metadata.last_modified.store(current_time, Ordering::SeqCst);
         self.metadata.last_modifier = "isdood";
-        self.timestamp.store(current_time, Ordering::SeqCst);
+        self.metadata.timestamp.store(current_time, Ordering::SeqCst);
     }
 }
 
