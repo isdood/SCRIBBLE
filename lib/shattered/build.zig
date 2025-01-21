6d6d658164b8ba7afc0e6a45e28fe4ddf85afa75
isdood <calebjdt@gmail.com>
2025-01-20 23:54:02 -0500

    dbg.

--- lib/shattered/build.zig
+++ lib/shattered/build.zig
@@ -4,24 +4,26 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    // Main library
     const lib = b.addStaticLibrary(.{
-        .name = "shattered",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .name = "scribble",
+        .root_source_file = .{ .cwd_relative = "src/cache.zig" },
         .target = target,
         .optimize = optimize,
     });
 
-    b.installArtifact(lib);
-
     // Tests
     const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "tests/main_test.zig" },
+        .root_source_file = .{ .cwd_relative = "tests/main_test.zig" },
         .target = target,
         .optimize = optimize,
     });
 
     const run_main_tests = b.addRunArtifact(main_tests);
 
+    // Create a test step
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&run_main_tests.step);
+
+    b.installArtifact(lib);
 }
