af8ee0766fb5c4dca188ce50bcd51f9511417470
isdood <calebjdt@gmail.com>
2025-01-24 21:52:45 -0500

    feat: integrate sparkle module with scribble framework
    - Add Crystal module definition
    - Configure module in Scribble framework
    - Set up proper module structure
    - Update module configuration
    
    Created: 2025-01-25 02:51:59
    Author: isdood

---
+++ mod.cr
@@ -0,0 +1,19 @@
+module Scribble
+  module Sparkle
+    VERSION = "1.0.0"
+    CREATED = "2025-01-25 02:51:59"
+    TENDER  = "isdood"
+
+    def self.root_path
+      File.dirname(__FILE__)
+    end
+
+    def self.pattern_path
+      File.join(root_path, "patterns")
+    end
+
+    def self.config_path
+      File.join(root_path, "config.sparkle")
+    end
+  end
+end
